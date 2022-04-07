ffe210d4cf0214c065c0ce3b69a180581080a9e7
Kseniia Kovalenko <kseniiakovalenko.94@gmail.com>
2022-04-07 08:24:50 +0300

    Add error display

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,1 +1,30 @@
 "use strict";
+
+const form = document.querySelector('#login');
+
+const loginData = {};
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    if(loginData.username === '') {
+        usernameInput.style.border = '1px solid red';
+        return;
+    }
+   
+    if(loginData.password === '') {
+        passwordInput.style.border = '1px solid red';
+        return;
+    }
+   
+    console.log(loginData.username);
+});
+
+const usernameInput = document.querySelector('[name="username"]');
+const passwordInput = document.querySelector('[name="password"]');
+
+function onInput(event) {
+    loginData[event.target.name] = event.target.value;
+}
+usernameInput.addEventListener('input', onInput);
+passwordInput.addEventListener('input', onInput);
